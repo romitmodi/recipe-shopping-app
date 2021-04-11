03e195fdc01ef3e23c0b55e6867e8ed165fa8330
Romit Modi <modiromit7@gmail.com>
2021-04-11 21:04:36 +0530

    ingredients linked to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,10 +6,36 @@ import { Recipe } from "./recipe.model";
 })
 export class RecipeService {
     recipeSelectedEvent = new EventEmitter<Recipe>();
-    
+
     private recipes: Array<Recipe> = [
-        new Recipe('Samosa', 'Indian Traditional Dish', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg'),
-        new Recipe('Chole', 'Indian Traditional Dish', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg')
+        new Recipe(
+            'Samosa',
+            'Indian Traditional Dish',
+            'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg',
+            [
+                {
+                    name: 'white flour',
+                    quantity: 1
+                },
+                {
+                    name: 'potatoes',
+                    quantity: 2
+                }
+            ]),
+        new Recipe(
+            'Chole',
+            'Indian Traditional Dish',
+            'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg',
+            [
+                {
+                    name: 'Chole',
+                    quantity: 100
+                },
+                {
+                    name: 'salt',
+                    quantity: 1
+                }
+            ])
     ];
 
     constructor() { }
