bb8dc7558d56988b7ba5ad1ff8f0895023927ec4
Romit Modi <modiromit7@gmail.com>
2021-05-06 11:40:13 +0530

    reactive form sync and save and cancel feature addition

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -10,15 +12,53 @@ export class RecipeEditComponent implements OnInit {
 
   name: string;
   editMode: boolean = false;
-  constructor(private activatedRoute: ActivatedRoute) { }
+
+  recipeDetailForm: FormGroup;
+
+  constructor(private route: Router,
+    private activatedRoute: ActivatedRoute,
+    private recipeService: RecipeService) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(
-      (param: Params) => {
-        this.name = param['name'];
-        this.editMode = param['name'] != null;
-      }
-    );
+    this.activatedRoute.params
+      .subscribe(
+        (param: Params) => {
+          this.name = param['name'];
+          this.editMode = param['name'] != null;
+          this.initForm();
+        }
+      );
   }
 
+  initForm() {
+    let recipeName;
+    let recipeDescription;
+    let imagePath;
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipeByName(this.name);
+      recipeName = recipe.name;
+      recipeDescription = recipe.description;
+      imagePath = recipe.imagePath;
+    }
+    this.recipeDetailForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(imagePath),
+      'description': new FormControl(recipeDescription),
+      'ingredients': new FormArray([])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.recipeDetailForm.value);
+    if (this.editMode) {
+      this.editMode = false;
+      this.route.navigate(['recipes', this.name]);
+    }
+  }
+
+  onCancel() {
+    this.editMode = false;
+    this.route.navigate(['recipes', this.name]);
+  }
 }
