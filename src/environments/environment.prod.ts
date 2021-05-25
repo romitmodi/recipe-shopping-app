ffac4edde8184d7016f86c5a6740f94fd9514de1
Romit Modi <modiromit7@gmail.com>
2021-05-25 23:44:10 +0530

    build readiness issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   backendUrl: 'https://shopping-recipe-app-backend-default-rtdb.asia-southeast1.firebasedatabase.app/',
-  authSignUpUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='
+  authSignUpUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
+  authLoginUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key='
 };
