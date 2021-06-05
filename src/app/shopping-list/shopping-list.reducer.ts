99454c8b790aae74f55fd6b95a6498a572dcfb61
Romit Modi <modiromit7@gmail.com>
2021-06-05 17:26:39 +0530

    reducer addition for shopping-list module

---
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -0,0 +1,12 @@
+import { Ingredient } from "../shared/model/ingredient.model";
+
+const initalState = {
+    ingredients: [
+        new Ingredient('Apple', 5),
+        new Ingredient('Tomatos', 10)
+    ]
+};
+
+export function shoppingListReducer(state, action) {
+
+}
