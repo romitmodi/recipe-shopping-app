7e43ee49605a90b8711951bd2dba05d1fdab2ea9
Romit Modi <modiromit7@gmail.com>
2021-06-05 17:36:02 +0530

    logic added in reducer

--- src/app/shopping-list/shopping-list.reducer.ts
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -1,3 +1,6 @@
+import { Action } from "@ngrx/store";
+import { throws } from "assert";
+import { throwError } from "rxjs";
 import { Ingredient } from "../shared/model/ingredient.model";
 
 const initalState = {
@@ -7,6 +10,14 @@ const initalState = {
     ]
 };
 
-export function shoppingListReducer(state, action) {
-
+export function shoppingListReducer(state = initalState, action: Action) {
+    switch (action.type) {
+        case "ADD_INGREDIENTS":
+            return {
+                ...state,
+                ingredients: [...state.ingredients, action]
+            };
+        default:
+            console.log('incorrect acction triggered, which is not supported by application');
+    }
 }
